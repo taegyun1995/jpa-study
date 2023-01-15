f5272edda2c98864bba6d4d93a9faa693b99a151
gyunny <needle12s@gmail.com>
2023-01-15 15:51:41 +0900

    feat : order fetch join select by paging

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,10 +9,10 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -58,39 +58,52 @@ public class OrderApiController {
         return result;
     }
 
-        @Getter
-        static class OrderDto {
-            private Long orderId;
-            private String name;
-            private LocalDateTime orderDate;
-            private OrderStatus orderStatus;
-            private Address address;
-            private List<OrderItemDto> orderItems;
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
 
-            public OrderDto(Order order) {
-                orderId = order.getId();
-                name = order.getMember().getName();
-                orderDate = order.getOrderDate();
-                orderStatus = order.getStatus();
-                address = order.getDelivery().getAddress();
-                orderItems = order.getOrderItems().stream()
-                        .map(oi -> new OrderItemDto(oi))
-                        .collect(Collectors.toList());
-            }
-        }
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
-        @Getter
-        static class OrderItemDto {
-            private String itemName;
-            private int orderPrice;
-            private int count;
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
 
-            public OrderItemDto(OrderItem orderItem) {
-                itemName = orderItem.getItem().getName();
-                orderPrice = orderItem.getItem().getPrice();
-                count = orderItem.getCount();
-            }
-        }
-
+        return result;
     }
 
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(oi -> new OrderItemDto(oi))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getItem().getPrice();
+            count = orderItem.getCount();
+        }
+    }
+
+}
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -98,12 +98,12 @@ public class OrderRepository {
     }
 
     public List<Order> findAllWithMemberDelivery() {
-        return em.createQuery("select o from Order o " +
+        return em.createQuery(
+                "select o from Order o " +
                         "join fetch o.member m " +
                         "join fetch o.delivery d", Order.class)
                 .getResultList();
     }
-
 
     public List<Order> findAllWithItem() {
         return em.createQuery(
@@ -114,4 +114,15 @@ public class OrderRepository {
                                 "join fetch oi.item i", Order.class)
                 .getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                    "select o from Order o " +
+                            "join fetch o.member m " +
+                            "join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
 }
